425d8e4d609e886d20f3b3ecde1d0c1c9ebb09cc
ThrzMy <thriziamay12@gmail.com>
2023-03-21 23:15:32 +0800

    added animations

--- js/script.js
+++ js/script.js
@@ -60,5 +60,18 @@ let darkModeIcon=document.querySelector('#darkMode-icon');
 darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('dark-mode');
-}
+};
+
+/*========== scroll reveal =========*/
+ScrollReveal({ 
+ /*  reset: true , */
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .certification-container, .portfolio-box, .recommended-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
   
